55715d02af20da2037fac3d4e97111a3afa38ed3
Pantelis Gerolemou <pantelisgerolemou06@gmail.com>
2025-05-07 23:06:55 +0300

    Initial Commit

---
+++ proj/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  proj
+//
+//  Created by Pantelis Gerolemou on 07/05/2025.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
